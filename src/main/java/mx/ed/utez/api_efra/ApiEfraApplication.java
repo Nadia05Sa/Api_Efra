2adaeadec2514a5835ac9b02dc16c88a62c7d8f7
Nadia05Sa <168792799+Nadia05Sa@users.noreply.github.com>
2024-12-11 00:12:30 -0600

    Add files via upload

---
+++ src/main/java/mx/ed/utez/api_efra/ApiEfraApplication.java
@@ -0,0 +1,13 @@
+package mx.ed.utez.api_efra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEfraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEfraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/ed/utez/api_efra/ApiEfraApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.ed.utez.api_efra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEfraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

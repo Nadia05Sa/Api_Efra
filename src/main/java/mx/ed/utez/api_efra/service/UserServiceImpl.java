5f894e3664021251ea0ca33b0460c63272ff74cd
Alison <20233tn221@utez.edu.mx>
2024-12-11 04:22:40 -0600

    mm

--- src/main/java/mx/ed/utez/api_efra/service/UserServiceImpl.java
+++ src/main/java/mx/ed/utez/api_efra/service/UserServiceImpl.java
@@ -1,4 +1,49 @@
 package mx.ed.utez.api_efra.service;
 
-public class UserServiceImpl {
+import mx.ed.utez.api_efra.model.DAO.UserDao;
+import mx.ed.utez.api_efra.model.Empleados;
+import mx.ed.utez.api_efra.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserDao userDao;
+    private User user;
+
+    @Override
+    public List<User> getUsers(){
+        return (List<User>)  userDao.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id){
+        Optional<User> user = userDao.findById(id);
+        return user.orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+    }
+
+    @Override
+    public User saveUser(User user){
+        return userDao.save(user);
+    }
+
+    @Override
+    public User updateUser(Long id){
+        User nuevoUser = getUserById(id);
+        nuevoUser.setId(user.getId());
+        return userDao.save(nuevoUser);
+    }
+
+    @Override
+    public boolean deleteUser(Long id){
+        if(userDao.existsById(id)){
+            userDao.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
